0db7a56fdd149a2bb0bf0753da59ce9ed2ef0bc0
taishi.nakase <pablo.aimar21vgmail.com>
2021-05-27 09:04:03 +0000

    add colum

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
       @tasks = Task.all
   end
 
   def show
-      @task = Task.find(params[:id])
   end
 
   def new
@@ -24,7 +25,6 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
@@ -50,7 +50,12 @@ class TasksController < ApplicationController
   private
 
   # Strong Parameter
-  def task_params
-      params.require(:task).permit(:content)
+  def set_task
+    @task = Task.find(params[:id])
   end
+
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+
 end
