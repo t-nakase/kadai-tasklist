1eef1a31e84bfb392abea8742abd8b2119cd9d88
taishi.nakase <pablo.aimar21vgmail.com>
2021-06-09 12:14:25 +0000

    kadai2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
+    else
+      if @current_user.id != params[:id].to_i
+        redirect_to login_url
+      end
     end
   end
 
