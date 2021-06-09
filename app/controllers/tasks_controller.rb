ccfbaecf4462cf21e143beb4685aaee1bb2aa939
taishi.nakase <pablo.aimar21vgmail.com>
2021-06-09 04:15:12 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,12 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks = Task.all
+      if logged_in?
+       @task = current_user.tasks.build
+       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      else
+       redirect_to login_path
+      end
   end
 
   def show
@@ -13,7 +18,7 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
       
       if @task.save
           flash[:success] = 'Task が正常に投稿されました'
